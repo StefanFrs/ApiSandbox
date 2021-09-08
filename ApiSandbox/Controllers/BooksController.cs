25226e091160d764686213098aa82c1523c44ee5
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-09-08 17:29:12 +0300

    Style cop added to books and weather forecast controllers

--- ApiSandbox/Controllers/BooksController.cs
+++ ApiSandbox/Controllers/BooksController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// <copyright file="BooksController.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ApiSandbox.Controllers
 {
+    /// <summary>
+    ///   <br />
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class BooksController : ControllerBase
@@ -18,47 +24,51 @@ namespace ApiSandbox.Controllers
         {
             this.booksService = booksService;
         }
-      
+
         // GET: api/<BooksController>
         [HttpGet]
         public IEnumerable<Book> Get()
         {
-            return booksService.Get();
+            return this.booksService.Get();
         }
 
         // GET api/<BooksController>/5
+
+        /// <summary>Gets the specified book by id.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>book object.</returns>
         [HttpGet("{id}")]
         public ActionResult Get(int id)
         {
-            try {
-                return Ok(booksService.Get(id));
-            }
-            catch (Exception e) 
+            try
             {
-                return NotFound();
+                return this.Ok(this.booksService.Get(id));
+            }
+            catch (Exception e)
+            {
+                return this.NotFound();
             }
         }
 
-     
         // POST api/<BooksController>
         [HttpPost]
         public void Post([FromBody] Book value)
         {
-            booksService.Post(value);
+            this.booksService.Post(value);
         }
 
         // PUT api/<BooksController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] Book value)
         {
-            booksService.Put(id, value);
+            this.booksService.Put(id, value);
         }
 
         // DELETE api/<BooksController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            booksService.Delete(id);
+            this.booksService.Delete(id);
         }
     }
 }
--- ApiSandbox/Controllers/WeatherForecastController.cs
+++ ApiSandbox/Controllers/WeatherForecastController.cs
@@ -16,7 +16,7 @@ namespace AspNetSandbox.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private const float KELVIN_CONST = 273.15f;
+        private const float KELVINCONST = 273.15f;
 
         /// <summary>
         /// Getting weather forecast for 5 days.
@@ -25,33 +25,28 @@ namespace AspNetSandbox.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=46.652010&lon=24.484990&exclude=hourly,minutely&appid=3b5027203e63534b989ac149d0c2ce31");
-            client.Timeout = -1;
+            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=46.652010&lon=24.484990&exclude=hourly,minutely&appid=3b5027203e63534b989ac149d0c2ce31")
+            {
+                Timeout = -1,
+            };
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
             Console.WriteLine(response.Content);
 
-            return ConvertResponseToWeatherForecast(response.Content);
-
+            return this.ConvertResponseToWeatherForecast(response.Content);
         }
 
         [NonAction]
 
         public IEnumerable<WeatherForecast> ConvertResponseToWeatherForecast(string content, int days = 5)
         {
-
             var json = JObject.Parse(content);
             var rng = new Random();
-
-
             return Enumerable.Range(1, days).Select(index =>
             {
-
                 var jsonDailyForecast = json["daily"][index];
                 var unixDateTime = jsonDailyForecast.Value<long>("dt");
                 var weatherSummary = jsonDailyForecast["weather"][0].Value<string>("main");
-
-
                 return new WeatherForecast
                 {
                     Date = DateTimeOffset.FromUnixTimeSeconds(unixDateTime).Date,
@@ -64,15 +59,14 @@ namespace AspNetSandbox.Controllers
 
         private static int ExtractCelsiusTemperatureFromDailyWeather(JToken jsonDailyForecast)
         {
-            return (int)Math.Round(jsonDailyForecast["temp"].Value<float>("day") - KELVIN_CONST);
+            return (int)Math.Round(jsonDailyForecast["temp"].Value<float>("day") - KELVINCONST);
         }
-
     }
 
-    //WeatherForecast for city 
+    // WeatherForecast for city
     public class WeatherForecastControllerCity : ControllerBase
     {
-        private const float KELVIN_CONST = 273.15f;
+        private const float KELVINCONST = 273.15f;
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
@@ -82,29 +76,21 @@ namespace AspNetSandbox.Controllers
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
             Console.WriteLine(response.Content);
-
-            return ConvertResponseToWeatherForecastCity(response.Content);
-
+            return this.ConvertResponseToWeatherForecastCity(response.Content);
         }
 
         [NonAction]
         public IEnumerable<WeatherForecast> ConvertResponseToWeatherForecastCity(string content, int days = 5)
         {
-
             var jsonCity = JObject.Parse(content);
             var rng = new Random();
-
-
             return Enumerable.Range(1, days).Select(index =>
             {
-
                 var nameCity = jsonCity["name"];
                 var lat = jsonCity["coord"]["lat"];
                 var lon = jsonCity["coord"]["lon"];
-
-
                 return new WeatherForecast
-                {  
+                {
                    latitude = lat,
                    longitude = lon,
                    name = nameCity,
@@ -112,9 +98,10 @@ namespace AspNetSandbox.Controllers
             })
             .ToArray();
         }
+
         private static int ExtractCelsiusTemperatureFromDailyWeather(JToken jsonDailyForecast)
         {
-            return (int)Math.Round(jsonDailyForecast["temp"].Value<float>("day") - KELVIN_CONST);
+            return (int)Math.Round(jsonDailyForecast["temp"].Value<float>("day") - KELVINCONST);
         }
     }
 }
