9197a294f3adab31755c8889ae50e52bdd2e5155
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-09-16 09:17:00 +0300

    Table row added in books.js

--- ApiSandbox/Controllers/BooksController.cs
+++ ApiSandbox/Controllers/BooksController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ApiSandbox.DTOs;
 using AutoMapper;
+using Microsoft.AspNetCore.SignalR;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 namespace ApiSandbox.Controllers
--- ApiSandbox/wwwroot/js/books.js
+++ ApiSandbox/wwwroot/js/books.js
@@ -1,4 +1,5 @@
-﻿"use strict"
+﻿
+"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/messagehub").build();
 
@@ -8,8 +9,30 @@ connection.start().then(function () {
     return console.error(err.toString());
 });
 
-connection.on("BookCreated", function (book) {
-    console.log("Book Created" + book.toString());
-    //here you have to insert html <tr> from table 
-    
+connection.on("AddedBook", (book) => {
+    console.log("Book added: " + JSON.stringify(book));
+    $("tbody")[0].append(`<tr>
+        <td>
+            ${book.title}added
+        </td>
+        <td>
+            ${book.author}
+        </td>
+        <td>
+            ${book.language}
+        </td>
+        <td>
+            <a href="/Books/Edit?id=${book.id}">Edit</a> |
+            <a href="/Books/Details?id=${book.id}">Details</a> |
+            <a href="/Books/Delete?id=${book.id}">Delete</a>
+        </td>
+    </tr>`)
+});
+
+connection.on("EditedBook", (book) => {
+    console.log("Edited book: " + JSON.stringify(book));
+});
+
+connection.on("DeletedBook", (book) => {
+    console.log("Deleted book: " + JSON.stringify(book));
 });
