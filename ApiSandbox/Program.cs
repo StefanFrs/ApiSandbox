c4e78dced5f95f834851473151ac74ba685157ea
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-09-23 08:54:25 +0300

    refactoring ConnectionOptions

--- ApiSandbox/Program.cs
+++ ApiSandbox/Program.cs
@@ -17,44 +17,37 @@ namespace ApiSandbox
             [Option('v', "Verbose", Required = false, HelpText = "Set output to verbose messages.")]
             public bool Verbose { get; set; }
 
-            public bool ConnectionString { get; internal set; }
-        }
-
-        public class ConnectionOptions
-        {
-            [Option('c', "Connection", Required = false, HelpText = "Set the connection")]
+            [Option('c', "ConnectionString", Required = false, HelpText = "Set connection String.")]
             public bool ConnectionString { get; set; }
         }
 
         public static int Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options, ConnectionOptions>(args)
+            Parser.Default.ParseArguments<Options>(args)
                   .WithParsed<Options>(o =>
                   {
-                      if (o.Verbose)
+                  if (o.Verbose)
+                  {
+                      Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                      Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                  }
+                  else
+                  {
+                      Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                      Console.WriteLine("Quick Start Example!");
+                  }
+
+                  if (o.ConnectionString)
                       {
-                          Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
-                          Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                          Console.WriteLine($"ConnectionString output enabled. Current Arguments: -c {o.ConnectionString}");
+                          Console.WriteLine("Quick Start Example! App is in Connection mode!");
                       }
                       else
                       {
-                          Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                          Console.WriteLine($"Current Arguments: -c {o.ConnectionString}");
                           Console.WriteLine("Quick Start Example!");
                       }
-
-                      Console.WriteLine(o.ConnectionString);
                   });
-                  //.WithParsed<ConnectionOptions>(c =>
-                  //  {
-                  //      if (o.Connection.ToString == "SqlServer")
-                  //      {
-                  //          Console.WriteLine(o.Connection);
-                  //      }
-                  //      else
-                  //      {
-                  //          Console.WriteLine("No SQL connection!");
-                  //      }
-                  //  });
             if (args.Length > 0)
             {
                 Console.WriteLine($"There are : {args.Length} args." );
